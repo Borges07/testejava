f1145d28f47dce2160efa3093654f39dd680b954
Matheus Borges Neves <matheus.neves@PSC.LOCAL>
2024-09-20 16:45:03 -0300

    top d+

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int a = 2;
+        int b = 3;
+        String soma = Integer.toString(a+b);
+        System.out.println("top d+ = " + soma);
+
+        
     }
 }
